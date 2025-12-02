8b072e45b432ccf1e9931d2b071146a36d710a8b
Sriparna Roy <90884880+SRIPARNAROY06@users.noreply.github.com>
2025-12-02 11:20:45 +0000

    superstore_part2

--- 02-12-2025/superstore.py
+++ 02-12-2025/superstore.py
@@ -55,4 +55,80 @@ total_quantity=df.groupby("Segment")["Quantity"].sum()
 shipping_days=df.groupby("Category")["ShippingDays"].mean()
 #print(shipping_days)
 
+#Example 27-31
+
+# df['Month'] = df['OrderDate'].dt.to_period('M')
+# pivot_sales = pd.pivot_table(df, index='Region', columns='Category', values='Sales', aggfunc='sum', fill_value=0)
+# print("Pivot 1: Sales by Region and Category\n", pivot_sales, "\n")
+#
+#
+# pivot_profit = pd.pivot_table(df, index='SubCategory', columns='Segment', values='Profit', aggfunc='sum', fill_value=0)
+# print("Pivot 2: Profit by SubCategory and Segment\n", pivot_profit, "\n")
+#
+#
+# pivot_returned = pd.pivot_table(df, index='Returned', columns='Region', values='OrderID', aggfunc='count', fill_value=0)
+# print("Pivot 3: Count of Orders by Returned Status and Region\n", pivot_returned, "\n")
+#
+#
+# pivot_avg_price = pd.pivot_table(df, index='Category', values='UnitPrice', aggfunc='mean')
+# print("Pivot 4: Average UnitPrice per Category\n", pivot_avg_price, "\n")
+#
+# pivot_quantity = pd.pivot_table(df, index='Month', columns='Region', values='Quantity', aggfunc='sum', fill_value=0)
+# print("Pivot 5: Quantity per Month\n", pivot_quantity, "\n")
+
+#Example 32-35
+
+segment_discount = pd.DataFrame({
+    'Segment': ['Consumer', 'Corporate', 'Home Office'],
+    'SegmentDiscount': [0.05, 0.08, 0.10]  # 5%, 8%, 10%
+})
+df = df.merge(segment_discount, on='Segment', how='left')
+#print(df)
+region_tax_table= pd.DataFrame({
+
+    'Region': ['West', 'East', 'Central', 'South'],
+    'RegionTax': [0.07, 0.06, 0.05, 0.08]  # 7%, 6%, 5%, 8%
+
+})
+df=df.merge(region_tax_table, on='Region', how='left')
+#print(df[["RegionTax","Region"]])
+cust_level_totals=df.groupby('CustomerID').agg(
+    CustomerTotalSales=('Sales', 'sum'),
+    CustomerTotalProfit=('Profit', 'sum')
+).reset_index()
+# df = df.merge(cust_level_totals, on='CustomerID', how='left')
+# print(df)
+
+pdt_level_profit=df.groupby('ProductName').agg(
+    ProductTotalSales=('Sales', 'sum'),
+    ProductTotalProfit=('Profit', 'sum')
+).reset_index()
+# df = df.merge(pdt_level_profit, on='ProductName', how='left')
+# print(df)
+
+#Example 36-40
+df["OrderYear"]=df["OrderDate"].dt.year
+df["OrderMonth"]=df["OrderDate"].dt.month
+df["OrderDay"]=df["OrderDate"].dt.day
+#print(df[['OrderDate', 'OrderYear', 'OrderMonth', 'OrderDay']].head())
+
+df['OrderDayOfWeek'] = df['OrderDate'].dt.day_name()
+#print(df["OrderDayOfWeek"].head())
+
+
+df['ShipDelayDays'] = (df['ShipDate'] - df['OrderDate']).dt.days
+orders_delayed = df[df['ShipDelayDays'] > 5]
+#print(orders_delayed)
+
+
+
+df['OrderMonth'] = df['OrderDate'].dt.strftime('%Y-%m')
+monthly_sales = df.groupby('OrderMonth', as_index=False)['Sales'].sum()
+
+fig = px.line(monthly_sales, x='OrderMonth', y='Sales',
+             title='Monthly Sales Trend', markers=True)
+fig.show()
+
+print("Orders shipped in more than 5 days:\n", orders_delayed[['OrderID','OrderDate','ShipDate','ShipDelayDays']], "\n")
+print("Monthly Sales Summary:\n", monthly_sales)
 
